e636cb25cdedd3713ddcff87a748eb93be6993bd
Wake1st <joeltwakefield@gmail.com>
2024-07-17 16:35:14 -0600

    init some project data

---
+++ ProjectManagementApp/Data/Project.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjectManagementApp.Data
+{
+    public class Project
+    {
+        public string Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public DateTime ProjectedStart { get; set; }
+        public DateTime ProjectedEnd { get; set; }
+        public DateTime ActualStart { get; set; }
+        public DateTime ActualEnd { get; set; }
+        public float TotalWorkHours { get; set; }
+
+        public Project(string name, string userId)
+        {
+            Id = Guid.NewGuid().ToString();
+            CreatedOn = DateTime.UtcNow;
+            CreatedBy = userId;
+            Name = name;
+        }
+    }
+}
---
+++ ProjectManagementApp/Data/ProjectDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectManagementApp.Data
+{
+    public class ProjectDbContext(DbContextOptions<ProjectDbContext> options) : DbContext(options)
+    {
+        public DbSet<Project>? Projects { get; set; }
+    }
+}
