002fad9a4ad83647bf209397d563b752f5ca48b1
Wake1st <joeltwakefield@gmail.com>
2024-07-18 14:33:27 -0600

    added project migrations

--- ProjectManagementApp/Data/ProjectDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace ProjectManagementApp.Data
-{
-    public class ProjectDbContext(DbContextOptions<ProjectDbContext> options) : DbContext(options)
-    {
-        public DbSet<Project>? Projects { get; set; }
-    }
-}
