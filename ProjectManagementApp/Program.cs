efbc2d994a2cd01ba5b5abcc1afa532f605f2154
Wake1st <joeltwakefield@gmail.com>
2024-07-22 12:15:16 -0600

    fixed mudblazor issue

--- ProjectManagementApp/Program.cs
+++ ProjectManagementApp/Program.cs
@@ -9,6 +9,7 @@ using ProjectManagementApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
