7292eab5acc3b8e9c6cf322a4bd13a6d24d0530c
Wake1st <joeltwakefield@gmail.com>
2024-07-23 18:48:08 -0600

    project owner can be viewed and updated

---
+++ ProjectManagementApp/ViewModels/ProjectOwnerSelection.cs
@@ -0,0 +1,27 @@
+﻿namespace ProjectManagementApp.ViewModels
+{
+    public class ProjectOwnerSelection
+    {
+        public ProjectOwnerSelection(string id, string? name)
+        {
+            Id = id;
+            Name = name ?? "NO USERNAME";
+        }
+
+        public readonly string Id;
+        public readonly string Name;
+
+        // Note: this is important so the MudSelect can compare ownerss
+        public override bool Equals(object o)
+        {
+            var other = o as ProjectOwnerSelection;
+            return other?.Name == Name;
+        }
+
+        // Note: this is important too!
+        public override int GetHashCode() => Name?.GetHashCode() ?? 0;
+
+        // Implement this for the Owner to display correctly in MudSelect
+        public override string ToString() => Name;
+    }
+}
