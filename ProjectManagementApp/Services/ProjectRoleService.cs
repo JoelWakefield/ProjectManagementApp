2e5a0d37ba335ad4b40ba7b05f5653e55c14be9c
Wake1st <joeltwakefield@gmail.com>
2024-07-25 11:49:06 -0600

    added project role service

--- ProjectManagementApp/Services/ProjectRoleService.cs
+++ ProjectManagementApp/Services/ProjectRoleService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProjectManagementApp.Data;
+using ProjectManagementApp.Models;
 
 namespace ProjectManagementApp.Services
 {
@@ -12,11 +13,51 @@ namespace ProjectManagementApp.Services
             this.dbContext = dbContext;
         }
 
-        public IEnumerable<ProjectRole> GetUserProjectRoles(string userId)
+        public IEnumerable<ProjectRole>? GetUserRoles(string userId)
         {
-            var userRoleIds = DbContext.ProjectUserRoles.Where(r => r.UserId == UserId).Select(r => r.RoleId);
+            var userRoles = dbContext.ProjectUserRoles.Where(r => r.UserId == userId).AsEnumerable();
+            if (userRoles.Any() == false)
+                return new List<ProjectRole>().AsEnumerable();
 
-            DbContext.ProjectRoles.Where(r => userRoleIds.Contains(r.Id));
+            var userRoleIds = userRoles.Select(r => r.RoleId);
+            return dbContext.ProjectRoles.Where(r => userRoleIds.Contains(r.Id)).AsEnumerable();
+        }
+
+        public IEnumerable<string> GetUserRoleNames(string userId)
+        {
+            var roles = GetUserRoles(userId);
+            if (roles?.Any() == false)
+                return new List<string>().AsEnumerable();
+            else
+                return roles!.Select(r => r.Name).ToList();
+        }
+
+        public void RemoveRoleFromUser(string userId, string roleName)
+        {
+            ProjectRole? projectRole = dbContext.ProjectRoles.FirstOrDefault(r => r.Name == roleName);
+
+            if (projectRole == null) { return; }
+
+            ProjectUserRole? projectUserRole = dbContext.ProjectUserRoles.FirstOrDefault(r => r.RoleId == projectRole!.Id && r.UserId == userId);
+            
+            if (projectUserRole != null)
+            {
+                dbContext.ProjectUserRoles.Remove(projectUserRole);
+                dbContext.SaveChanges();
+            }
+        }
+
+        public void AddRoleToUser(string userId, string roleName)
+        {
+            ProjectRole? projectRole = dbContext.ProjectRoles.FirstOrDefault(r => r.Name == roleName);
+            if (projectRole == null) { return; }
+            
+            dbContext.ProjectUserRoles.Add(new ProjectUserRole { 
+                UserId = userId, 
+                RoleId = projectRole!.Id 
+            });
+
+            dbContext.SaveChanges();
         }
     }
 }
