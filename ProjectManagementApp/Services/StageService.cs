43f5c4b99510612fe4b22cc2e52837bd94d5f013
Wake1st <joeltwakefield@gmail.com>
2024-08-01 15:49:40 -0600

    adding stage pages

---
+++ ProjectManagementApp/Services/StageService.cs
@@ -0,0 +1,17 @@
+﻿using ProjectManagementApp.Data;
+
+namespace ProjectManagementApp.Services
+{
+    public class StageService(ApplicationDbContext dbContext)
+    {
+        private ApplicationDbContext dbContext = dbContext;
+
+        public IEnumerable<Stage> GetAllStages() => dbContext.Stages;
+
+        public async Task CreateStageAsync(string name)
+        {
+            await dbContext.Stages.AddAsync(new Stage { Name = name });
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
