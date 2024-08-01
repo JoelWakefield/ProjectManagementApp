844867c53723fb52bc056e9b6518fd48c8f13a54
Wake1st <joeltwakefield@gmail.com>
2024-08-01 15:49:34 -0600

    adding phase pages

--- ProjectManagementApp/Program.cs
+++ ProjectManagementApp/Program.cs
@@ -22,6 +22,8 @@ builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuth
 builder.Services.AddScoped<IProjectOwnerService,ProjectOwnerService>();
 builder.Services.AddScoped<IProjectRoleService,ProjectRoleService>();
 builder.Services.AddScoped<IProjectService,ProjectService>();
+builder.Services.AddScoped<PhaseService>();
+builder.Services.AddScoped<StageService>();
 
 builder.Services.AddAuthentication(options =>
     {
---
+++ ProjectManagementApp/Services/PhaseService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectManagementApp.Data;
+
+namespace ProjectManagementApp.Services
+{
+    public class PhaseService(ApplicationDbContext dbContext)
+    {
+        private ApplicationDbContext dbContext = dbContext;
+
+        public IEnumerable<Phase> GetAllPhases() => dbContext.Phases;
+        public async Task<Phase?> GetPhaseAsync(string id) => await dbContext.Phases.FirstOrDefaultAsync(p => p.Id == id);
+        public async Task UpdatePhaseAsync(Phase phase)
+        {
+            Phase? existingPhase = await GetPhaseAsync(phase.Id);
+            if (existingPhase != null)
+            {
+                existingPhase = phase;
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
