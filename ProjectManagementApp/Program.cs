c123e44ba5e6fad03b7b8e9d71de6810f1430cdb
Wake1st <joeltwakefield@gmail.com>
2024-08-06 14:00:44 -0600

    added phase owner service

--- ProjectManagementApp/Program.cs
+++ ProjectManagementApp/Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddScoped<IProjectOwnerService,ProjectOwnerService>();
 builder.Services.AddScoped<IProjectRoleService,ProjectRoleService>();
 builder.Services.AddScoped<IProjectService,ProjectService>();
 builder.Services.AddScoped<IPhaseService,PhaseService>();
+builder.Services.AddScoped<IPhaseOwnerService,PhaseOwnerService>();
 builder.Services.AddScoped<IStageService,StageService>();
 
 builder.Services.AddAuthentication(options =>
