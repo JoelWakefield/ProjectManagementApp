a0dcdf36c0ced02a9e0d7a47b5ab1feef373fc36
Wake1st <joeltwakefield@gmail.com>
2024-08-08 19:20:12 -0600

    added phase assignment page

---
+++ ProjectManagementApp/Services/PhaseAssignmentService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagementApp.Data;
+using ProjectManagementApp.ViewModels;
+
+namespace ProjectManagementApp.Services
+{
+	public interface IPhaseAssignmentService
+	{
+		Task<Dictionary<string,PhaseAssignedRecord>> GetPhaseAssignedRecordsAsync();
+		Task CreatePhaseAssignmentAsync(string phaseId, string userId, bool assigned);
+	}
+
+	public class PhaseAssignmentService(ApplicationDbContext dbContext, UserManager<ApplicationUser> userManager) : IPhaseAssignmentService
+	{
+        private ApplicationDbContext dbContext = dbContext;
+		private UserManager<ApplicationUser> userManager = userManager;
+
+		public async Task<Dictionary<string,PhaseAssignedRecord>> GetPhaseAssignedRecordsAsync()
+		{
+			Dictionary<string,PhaseAssignedRecord> records = new Dictionary<string, PhaseAssignedRecord>();
+
+			IEnumerable<ApplicationUser> users = await userManager.Users.ToListAsync();
+			IEnumerable<Phase> phases = dbContext.Phases;
+
+			foreach (var phase in phases)
+			{
+				PhaseAssignedRecord record = new PhaseAssignedRecord(phase.Id,phase.Name);
+				foreach (var user in users)
+				{
+					PhaseAssignment? assignment = await dbContext.PhaseAssignments
+						.Where(p => p.PhaseId == phase.Id && p.UserId == user.Id)
+						.OrderByDescending(p => p.CreatedOn)
+						.FirstOrDefaultAsync();
+
+					if (assignment == null)
+						record.Assignments.Add(user.Id, false);
+					else
+						record.Assignments.Add(assignment.UserId, assignment.Assigned);
+				}
+
+				records.Add(phase.Id, record);
+			}
+
+			return records;
+		}
+
+		public async Task CreatePhaseAssignmentAsync(string phaseId, string userId, bool assigned)
+		{
+			await dbContext.PhaseAssignments.AddAsync(new PhaseAssignment()
+			{
+				PhaseId = phaseId,
+				UserId = userId,
+				Assigned = assigned
+			});
+			await dbContext.SaveChangesAsync();
+		}
+	}
+}
---
+++ ProjectManagementApp/ViewModels/PhaseAssignedRecord.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjectManagementApp.ViewModels
+{
+	/// <summary>
+	/// Keeps a record of which users are assigned to a specific phase
+	/// </summary>
+	public class PhaseAssignedRecord
+	{
+		public string PhaseId { get; set; }
+		public string Name { get; set; }
+        public Dictionary<string, bool> Assignments { get; set; }
+
+		public PhaseAssignedRecord(string phaseId, string name)
+		{
+			PhaseId = phaseId;
+			Name = name;
+			Assignments = new Dictionary<string, bool>();
+		}
+    }
+}
