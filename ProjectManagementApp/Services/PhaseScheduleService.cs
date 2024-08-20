99675a5c43f0226643e7cc977bcd1baaa5b09810
Wake1st <joeltwakefield@gmail.com>
2024-08-20 16:05:36 -0600

    better gantt chart

--- ProjectManagementApp/Services/PhaseScheduleService.cs
+++ ProjectManagementApp/Services/PhaseScheduleService.cs
@@ -6,7 +6,8 @@ namespace ProjectManagementApp.Services
     public interface IPhaseScheduleService
     {
         IEnumerable<PhaseSchedule> GetSchedules();
-        Task<PhaseSchedule?> GetSchedule(string phaseId);
+        Task<PhaseSchedule?> GetScheduleAsync(string phaseId);
+        Task CreateScheduleAsync(PhaseSchedule schedule);
     }
 
     public class PhaseScheduleService(ApplicationDbContext dbContext) : IPhaseScheduleService
@@ -15,11 +16,17 @@ namespace ProjectManagementApp.Services
 
         public IEnumerable<PhaseSchedule> GetSchedules() => dbContext.PhaseSchedules;
 
-        public async Task<PhaseSchedule?> GetSchedule(string phaseId)
+        public async Task<PhaseSchedule?> GetScheduleAsync(string phaseId)
         {
             return await dbContext.PhaseSchedules
                 .OrderByDescending(s => s.CreatedOn)
                 .FirstOrDefaultAsync(s => s.PhaseId == phaseId);
+        }
+
+        public async Task CreateScheduleAsync(PhaseSchedule schedule)
+        {
+            await dbContext.PhaseSchedules.AddAsync(schedule);
+            await dbContext.SaveChangesAsync();
         }
     }
 }
