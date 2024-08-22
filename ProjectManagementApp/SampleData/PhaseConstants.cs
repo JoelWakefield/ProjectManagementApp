c9c959a9517ebafddd1913841c4be460fb4e7cce
Wake1st <joeltwakefield@gmail.com>
2024-08-22 15:10:06 -0600

    phase names are easier to recognise

--- ProjectManagementApp/SampleData/PhaseConstants.cs
+++ ProjectManagementApp/SampleData/PhaseConstants.cs
@@ -6,39 +6,39 @@ namespace ProjectManagementApp.SampleData
     {
         public static Phase SimplePlanning { get; set; } = new Phase
         {
-            Name = "SimplePlanning",
+            Name = "Simple Planning",
             Description = "a complete phase for simple project",
             ProjectId = Projects.SimpleProject.Id,
         };
         public static Phase SimpleSetup { get; set; } = new Phase
         {
-            Name = "SimpleSetup",
+            Name = "Simple Setup",
             Description = "a review phase for simple project",
             ProjectId = Projects.SimpleProject.Id,
         };
         public static Phase SimpleDataEntry { get; set; } = new Phase
         {
-            Name = "SimpleDataEntry",
+            Name = "Simple Data Entry",
             Description = "an in progress phase for simple project",
             ProjectId = Projects.SimpleProject.Id,
         };
         public static Phase SimpleQA { get; set; } = new Phase
         {
-            Name = "SimpleQA",
+            Name = "Simple QA",
             Description = "a todo phase for simple project",
             ProjectId = Projects.SimpleProject.Id,
             Priority = Priority.High,
         };
         public static Phase SimpleNotifyCompletion { get; set; } = new Phase
         {
-            Name = "SimpleNotifyCompletion",
+            Name = "Simple Notify Completion",
             Description = "another todo phase for simple project",
             ProjectId = Projects.SimpleProject.Id,
             Priority = Priority.Low,
         };
         public static Phase SimplePostAnalytics { get; set; } = new Phase
         {
-            Name = "SimplePostAnalytics",
+            Name = "Simple Post Analytics",
             Description = "a backlog phase for simple project",
             ProjectId = Projects.SimpleProject.Id,
         };
