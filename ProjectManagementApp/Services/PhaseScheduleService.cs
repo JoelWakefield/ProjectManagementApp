1824eaafd91ad45f5190bc061910843fffa7881a
Wake1st <joeltwakefield@gmail.com>
2024-08-22 14:57:42 -0600

    updated service to handle more querying

--- ProjectManagementApp/Services/PhaseScheduleService.cs
+++ ProjectManagementApp/Services/PhaseScheduleService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProjectManagementApp.Data;
+using ProjectManagementApp.ViewModels;
 
 namespace ProjectManagementApp.Services
 {
@@ -7,14 +8,37 @@ namespace ProjectManagementApp.Services
     {
         IEnumerable<PhaseSchedule> GetSchedules();
         Task<PhaseSchedule?> GetScheduleAsync(string phaseId);
+        Task<IEnumerable<GanttItem>> GetGanttItems();
         Task CreateScheduleAsync(PhaseSchedule schedule);
     }
 
-    public class PhaseScheduleService(ApplicationDbContext dbContext) : IPhaseScheduleService
+    public class PhaseScheduleService(ApplicationDbContext dbContext, IPhaseService phaseService) : IPhaseScheduleService
     {
         private ApplicationDbContext dbContext = dbContext;
+        private IPhaseService phaseService = phaseService;
 
         public IEnumerable<PhaseSchedule> GetSchedules() => dbContext.PhaseSchedules;
+
+        /// <summary>
+        /// Returns all schedules in the form of a gantt chart item.
+        /// TODO: only check for items which would be displayed on the chart.
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<GanttItem>> GetGanttItems()
+        {
+            var phases = phaseService.GetAllPhases().ToList();
+            var items = new List<GanttItem>();
+
+            foreach (var phase in phases)
+            {
+                var schedule = await GetScheduleAsync(phase.Id);
+
+                if (schedule != null)
+                    items.Add(new GanttItem(schedule, phase.Name));
+            }
+
+            return items.AsEnumerable();
+        }
 
         public async Task<PhaseSchedule?> GetScheduleAsync(string phaseId)
         {
---
+++ ProjectManagementApp/ViewModels/GanttItem.cs
@@ -0,0 +1,30 @@
+﻿using ProjectManagementApp.Data;
+
+namespace ProjectManagementApp.ViewModels
+{
+    public class GanttItem
+    {
+        public GanttItem(PhaseSchedule schedule, string name)
+        {
+            PhaseId = schedule.PhaseId;
+            Name = name;
+            Start = schedule.Start;
+            End = schedule.End;
+        }
+
+        public string PhaseId { get; set; }
+        public string Name { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+
+    public static class GanttItemExtensions
+    {
+        private static DateTime Today { get => DateTime.UtcNow.Date; }
+        private static DateTime ChartEnd(int daysOnChart) => Today.AddDays(daysOnChart);
+        public static int FromStart(this GanttItem item) => item.Start > Today ? (item.Start - Today).Days : 0;
+        public static int ToEnd(this GanttItem item, int daysOnChart) => ChartEnd(daysOnChart) > item.End ? (ChartEnd(daysOnChart) - item.End).Days : 0;
+        public static bool OnChart(this GanttItem item, int daysOnChart) => !(Today > item.End || ChartEnd(daysOnChart) < item.Start);
+        public static int Duration(this GanttItem item, int daysOnChart) => daysOnChart - item.FromStart() - item.ToEnd(daysOnChart);
+    }
+}
