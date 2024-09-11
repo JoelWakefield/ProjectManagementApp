7e0b73dacdcdedb289222702f48fc85ea4712ed3
Wake1st <joeltwakefield@gmail.com>
2024-09-11 12:01:25 -0600

    data migrations for entity adjustments

---
+++ ProjectManagementApp/Migrations/20240911174600_ImprovementsAndAttributes.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectManagementApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImprovementsAndAttributes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ArchiveId",
+                table: "Stages");
+
+            migrationBuilder.DropColumn(
+                name: "ArchiveId",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "ArchiveId",
+                table: "ProjectRoles");
+
+            migrationBuilder.DropColumn(
+                name: "ArchiveId",
+                table: "PhaseSchedules");
+
+            migrationBuilder.DropColumn(
+                name: "ArchiveId",
+                table: "Phases");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Projects",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "Projects",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Phases",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ArchiveId",
+                table: "Stages",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Projects",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "Projects",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ArchiveId",
+                table: "Projects",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ArchiveId",
+                table: "ProjectRoles",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ArchiveId",
+                table: "PhaseSchedules",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Phases",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ArchiveId",
+                table: "Phases",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
