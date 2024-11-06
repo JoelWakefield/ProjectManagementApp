9d291849d44a8b5e1af872d5560abb207341b664
Wake1st <joeltwakefield@gmail.com>
2024-11-05 17:26:59 -0700

    added react router WIP

--- ReactApp/reactapp.client/src/App.jsx
+++ ReactApp/reactapp.client/src/App.jsx
@@ -1,13 +1,10 @@
-import Users from './components/Users';
-import ProjectRoles from './components/ProjectRoles';
 import './App.css';
 
 function App() {
     return (
         <div>
             <h1 id="tableLabel">Dashboard</h1>
-            <Users />
-            <ProjectRoles />
+            <p>This page will have helpful components for project management.</p>
         </div>
     );
 
--- ReactApp/reactapp.client/src/main.jsx
+++ ReactApp/reactapp.client/src/main.jsx
@@ -1,10 +1,44 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx'
+import ProjectRoles from './routes/ProjectRoles';
+import Users from './routes/users';
+import './index.css'
+import User, { loader as userLoader } from './components/User.jsx';
+import ErrorPage from './error-page';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/roles",
+    element: <ProjectRoles />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/users",
+    element: <Users />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/users/:userId",
+        element: <User />,
+        loader: userLoader,
+        errorElement: <ErrorPage />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
