7ff5436b74f747c4f6869184cb30f967be2a084b
Wake1st <joeltwakefield@gmail.com>
2024-11-11 18:27:37 -0700

    cleanup

--- ReactApp/reactapp.client/src/main.jsx
+++ ReactApp/reactapp.client/src/main.jsx
@@ -12,15 +12,6 @@ import User, { loader as userLoader } from './components/User';
 import ErrorPage from './error-page';
 import './index.css'
 
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path="/" element={<Root />} >
-//       <Route index element={<Home />} />
-//       <Route path="users" element={<Users />} />
-//       <Route path="roles" element={<ProjectRoles />} />
-//     </Route>
-//   )
-// );
 const router = createBrowserRouter([
   {
     path: "/",
