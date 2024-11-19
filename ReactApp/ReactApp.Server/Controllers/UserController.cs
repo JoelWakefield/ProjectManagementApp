b7a273ef3b601dbb31437539d2e163965062cf9a
Wake1st <joeltwakefield@gmail.com>
2024-11-19 15:40:36 -0700

    project role update backend

--- ReactApp/ReactApp.Server/Controllers/UserController.cs
+++ ReactApp/ReactApp.Server/Controllers/UserController.cs
@@ -42,9 +42,28 @@ namespace ReactApp.Server.Controllers
                     return NotFound(new { message = $"User with id ({id}) not found." });
                 }
 
+                //  update flat user data
                 user.Name = request.Name;
-                await dbContext.SaveChangesAsync();
 
+                //  update projected user data
+                foreach(KeyValuePair<string, bool> role in request.ProjectRoles)
+                {
+                    var projectRole = dbContext.ProjectRoles.Single(r => r.Name == role.Key);
+                    if (projectRole == null)
+                    {
+                        return NotFound(new { message = $"Project Role ({role.Key}) not found." });
+                    }
+
+                    if (role.Value && !user.ProjectRoles.Any(r => r.Name == role.Key)) {
+                        //  add role if requested and not currently listed
+                        user.ProjectRoles.Add(projectRole);
+                    } else if (!role.Value && user.ProjectRoles.Any(r => r.Name == role.Key)) {
+                        //  remove role if requested and currently listed
+                        user.ProjectRoles.Remove(projectRole);
+                    }
+                }
+
+                await dbContext.SaveChangesAsync();
                 return Ok(new { message = $" Todo Item  with id ({id}) successfully updated" });
             }
             catch (Exception ex)
