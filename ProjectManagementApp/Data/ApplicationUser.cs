027143f688d85ebf405aa743ff71d76cffbeb763
Wake1st <joeltwakefield@gmail.com>
2025-02-07 18:07:30 -0700

    removed identity, blazor is working again

--- ProjectManagementApp/Data/ApplicationUser.cs
+++ ProjectManagementApp/Data/ApplicationUser.cs
@@ -1,10 +1,33 @@
-using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjectManagementApp.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
+    [Table("AspNetUsers")]
+    public class ApplicationUser
     {
+        public ApplicationUser() { }
+
+        public ApplicationUser(string userName) : this()
+        {
+            UserName = userName;
+        }
+
+        public string Id { get; set; } = default!;
+        public string? UserName { get; set; }
+        public string? NormalizedUserName { get; set; }
+        public string? Email { get; set; }
+        public string? NormalizedEmail { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string? PasswordHash { get; set; }
+        public string? SecurityStamp { get; set; }
+        public string? ConcurrencyStamp { get; set; } = Guid.NewGuid().ToString();
+        public string? PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+        public DateTimeOffset? LockoutEnd { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public int AccessFailedCount { get; set; }
     }
 
 }
